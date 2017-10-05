d11158af0116e13619cacd0c422c48e3488f2a6a
Maikiely Malini Gomes <31906046+Maikiely@users.noreply.github.com>
2017-10-05 00:05:21 -0300

    Página de login

---
+++ login.ts
@@ -0,0 +1,65 @@
+import { PerfilPage } from './../perfil/perfil';
+import { User } from './../../class/User';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+//apenas rest sem provider mas da pau?
+import { RestProvider } from '../../providers/rest/rest';
+import { CadastPage } from '../cadast/cadast';
+
+
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+	usuarios: string[];
+	errorMessage: string;
+  public user ={};
+  userDetails : any;
+  responseData: any;
+  public usuarioLogin = new User();
+  public data1 = JSON.parse(localStorage.getItem('userData'))
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public rest: RestProvider,public alertCtrl: AlertController) {
+    // console.log(data1);
+    // this.userDetails = data1.userData;
+    // console.log(data1.email);
+    // console.log(data1.senha);
+    // this.userPostData.email = this.userDetails.email;
+    // this.userPostData.senha = this.userDetails.senha;
+    // console.log(this.userPostData.senha)
+  }
+
+	soontm(){
+		alert("Essa opção estará disponível em breve!");
+	}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  Validar(){ //verifica se o usuario se encontra no banco.
+    if(this.usuarioLogin.email === this.data1.email && this.usuarioLogin.senha === this.data1.senha){
+      this.navCtrl.push(PerfilPage)
+    }else{
+      this.showAlert();
+    }
+  }
+  
+  showAlert() { // alerta para erro de login
+    let alert = this.alertCtrl.create({
+      title: 'Erro',
+      subTitle: 'Não foi possivel logar, login ou senha incorreto!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  public moveTo(){
+    this.navCtrl.push(CadastPage);
+  }
+
+}
